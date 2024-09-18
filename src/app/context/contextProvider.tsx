3e45a17f22e4d5905c4e8af35eb4d5a887d08447
seanolero <seanolero@outlook.com>
2024-09-18 14:06:58 +0300

    Show loading screen during event data fetch
    Added the `LoadingScreen` component to display a loading screen while event data is being fetched. This ensures users have visual feedback when data is loading, enhancing the user experience by preventing empty or partially loaded content displays.

--- src/app/context/contextProvider.tsx
+++ src/app/context/contextProvider.tsx
@@ -3,11 +3,18 @@ import React, {useEffect} from "react";
 import Header from "@/components/navigation/header";
 import Footer from "@/components/navigation/footer";
 import {usePathname} from "next/navigation";
-import {fetchPopular, fetchPromoted, fetchUpcoming, selectEventsCountry} from "@/data/slices/eventsSlice";
+import {
+    fetchPopular,
+    fetchPromoted,
+    fetchUpcoming,
+    selectEventsCountry,
+    selectEventsLoading
+} from "@/data/slices/eventsSlice";
 import {useAppDispatch, useAppSelector} from "@/hooks/hooks";
 import {checkUser, selectCurrentUser} from "@/data/slices/authSlice";
 import {fetchExchangeRates} from "@/data/slices/cartSlice";
 import {fetchTickets} from "@/data/slices/ticketsSlice";
+import LoadingScreen from "@/components/LoadingScreen";
 
 
 export default function ContextProvider({children}: { children: React.ReactNode }) {
@@ -15,6 +22,7 @@ export default function ContextProvider({children}: { children: React.ReactNode 
     const dispatch = useAppDispatch();
     const country = useAppSelector(selectEventsCountry);
     const user = useAppSelector(selectCurrentUser);
+    const loading = useAppSelector(selectEventsLoading);
     useEffect(() => {
         dispatch(fetchUpcoming());
         dispatch(fetchPopular());
@@ -26,11 +34,14 @@ export default function ContextProvider({children}: { children: React.ReactNode 
     }, [country]);
 
     useEffect(() => {
-        if (user){
+        if (user) {
             dispatch(fetchTickets());
         }
     }, [dispatch, user]);
 
+    if (loading) {
+        return <LoadingScreen/>;
+    }
     return <div>
         {pathname !== '/login' && <Header/>}
         <div className={`${pathname !== '/' && pathname !== '/login' && 'pt-[4.5rem]'}`}>
